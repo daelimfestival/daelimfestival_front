ad5a41ec5b4d4f18d59087e57fa4d80e1e6ca897
leesh9098 <leesh9098@naver.com>
2021-09-30 22:22:32 +0800

    Daelim Festival

--- src/common/header.js
+++ src/common/header.js
@@ -4,6 +4,7 @@ import Sidebar from "react-sidebar";
 import { IconContext } from "react-icons";
 import { AiFillHome, AiOutlineMenu } from "react-icons/ai";
 import { Link } from "react-router-dom";
+// import * as fnc from "../common/commonFunc.js";
 import './header.css';
 
 class Header extends React.Component {
@@ -11,14 +12,52 @@ class Header extends React.Component {
         super(props);
         this.state = {
             sidebarOpen: false,
-            title: "Daelim Festival"
+            title: "Daelim Festival",
+            token: "",
+            loginText: ""
         };
         this.onSetSidebarOpen = this.onSetSidebarOpen.bind(this);
-
+        // this.sessionYN = this.sessionYN.bind(this);
     }
 
     onSetSidebarOpen(open) {
         this.setState({ sidebarOpen: open });
+    }
+
+    onChangeToken(e) {
+        this.setState({ token: sessionStorage.getItem('token') });
+        console.log(this.state.token);
+    }
+
+    // getData() {
+    //     let current_url = location.href;
+    //     let json_data = '{"current_url":"' + current_url + '","id":' + inputId + ',"password":"' + inputPassword + '"}';
+
+    //     let json = btoa(encodeURIComponent(json_data));
+
+    //     fnc.executeQuery({
+    //         url: "action/member/login.php",
+    //         data: {
+    //             json: json
+    //         },
+    //         success: (res) => {
+    //             sessionStorage.setItem("token", res.token);
+    //             location.replace("/");
+    //         },
+    //     });
+    // }
+
+    // onSubmit(e) {
+    //     e.preventDefault();
+    //     getData();
+    // }
+
+    sessionYN() {
+        if (this.state.token) {
+            this.setState({ loginText: "로그인" });
+        } else {
+            this.setState({ loginText: "로그아웃" });
+        }
     }
 
     render() {
@@ -37,7 +76,7 @@ class Header extends React.Component {
                                 <p className="sidebar">대림게임토너먼트</p>
                             </Link>
                             <Link to="/Login">
-                                <p className="sidebar loginpage">로그인</p>
+                                <p className="sidebar loginpage">{this.state.token}</p>
                             </Link>
                         </div>
                     }
