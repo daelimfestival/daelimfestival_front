681e5d3e98390d8d7a805d923677025f374dc2d7
WonDaeHo <77261907+WonDaeHo@users.noreply.github.com>
2021-10-03 16:17:13 +0900

    2021/10/3
    Video.js commit

---
+++ src/routes/Video.js
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import "./Contest.css";
+import ReactPlayer from 'react-player';
+import video1 from './img/video1.png';
+
+class Video extends Component{
+    render(){
+        const video = [
+            'Video1','Video2','Video3','Video4'
+        ];
+        /*const contentList=video.map((videos)=>(<div className="contentList">{videos}</div>
+        ));*/
+        return (
+            <div class="contestBox">
+                <div>
+                    <img src={video1} alt=""/>
+                    <p>[대림 시사콜콜] 대림대 현 상황은? -cultureLand@naver.com-</p>
+                    <span>대림대 코로나속에서 현 상황을 문상기자가 전한다!</span>
+                </div>
+
+            </div>
+        );
+    }
+}
+export default Video;
