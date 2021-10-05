ec795e01714eaa6799f12a2e44329477ad42c640
SeoEunGi0802 <lolove08@naver.com>
2021-10-05 17:18:17 +0900

    1005 commit
    공용 코드 : axios 코드 수정
    gitignore 내용 수정- 포함된 폴더 삭제

--- src/common/commonFunc.js
+++ src/common/commonFunc.js
@@ -18,13 +18,7 @@ export const executeQuery = ({
     })
         .then((res) => {
             if (res.data.response === "error") {
-                if (error) {
-                    error(res.data);
-                } else if (res.data.msg === "이미 로그인하셨습니다.") {
-                    success(res.data);
-                } else {
-                    alert(res.data.msg);
-                }
+                alert(res.data.msg);
             } else if (res.data.response === "fail") {
                 alert("서버접속에 실패하였습니다. 관리자에게 문의해주시기 바랍니다.");
             } else if (res.data.response === "ok") {
