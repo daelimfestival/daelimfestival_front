85286862eee91859013c11e551406984ab68196b
WonDaeHo <77261907+WonDaeHo@users.noreply.github.com>
2021-10-05 18:02:56 +0900

    2021/10/05
    cartoon.js

--- src/routes/Cartoon.js
+++ src/routes/Cartoon.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import "./Contest.css";
 import ReactPlayer from 'react-player';
-import cartoon1 from '../images/cartoon1.png';
-import cartoon2 from '../images/cartoon2.png';
-import cartoon3 from '../images/cartoon3.png';
-import cartoon4 from '../images/cartoon4.png';
+import cartoon1 from './img/cartoon1.png';
+import cartoon2 from './img/cartoon2.png';
+import cartoon3 from './img/cartoon3.png';
+import cartoon4 from './img/cartoon4.png';
 import Modal from 'react-modal';
 import { AiOutlineClose } from "react-icons/ai";
+import { FaRegHeart } from "react-icons/fa";
+import { FaHeart } from "react-icons/fa";
 import { IconContext } from "react-icons";
 import loginModal from "../common/loginModal";
 
@@ -38,7 +40,6 @@ const modalStyles = {
 const modalStyle2 = {
 
 }
-
 class Cartoon extends Component{
     constructor(props) {
         super(props);
@@ -47,7 +48,11 @@ class Cartoon extends Component{
             showModal2: false,
             showModal3: false,
             showModal4: false,
-            showModal5: false
+            showModal5: false,
+            isChecked1: false,
+            isChecked2: false,
+            isChecked3: false,
+            isChecked4: false,
         };
 
         this.handleOpenModal1 = this.handleOpenModal1.bind(this);
@@ -55,7 +60,76 @@ class Cartoon extends Component{
         this.handleOpenModal3 = this.handleOpenModal3.bind(this);
         this.handleOpenModal4 = this.handleOpenModal4.bind(this);
         this.handleOpenModal5 = this.handleOpenModal5.bind(this);
+        this.onclick1 = this.onclick1.bind(this);
+        this.onclick2 = this.onclick2.bind(this);
+        this.onclick3 = this.onclick3.bind(this);
+        this.onclick4 = this.onclick4.bind(this);
         this.handleCloseModal = this.handleCloseModal.bind(this);
+    }
+
+    onclick1(){
+        this.state.isChecked1 ?
+            this.setState({
+                isChecked1: false,
+                isChecked2: false,
+                isChecked3: false,
+                isChecked4: false,
+            })
+            :
+            this.setState({
+                isChecked1: true,
+                isChecked2: false,
+                isChecked3: false,
+                isChecked4: false,
+            });
+    }
+    onclick2(){
+        this.state.isChecked2 ?
+            this.setState({
+                isChecked1: false,
+                isChecked2: false,
+                isChecked3: false,
+                isChecked4: false,
+            })
+            :
+            this.setState({
+                isChecked1: false,
+                isChecked2: true,
+                isChecked3: false,
+                isChecked4: false,
+            });
+    }
+    onclick3(){
+        this.state.isChecked3 ?
+            this.setState({
+                isChecked1: false,
+                isChecked2: false,
+                isChecked3: false,
+                isChecked4: false,
+            })
+            :
+            this.setState({
+                isChecked1: false,
+                isChecked2: false,
+                isChecked3: true,
+                isChecked4: false,
+            });
+    }
+    onclick4(){
+        this.state.isChecked4 ?
+            this.setState({
+                isChecked1: false,
+                isChecked2: false,
+                isChecked3: false,
+                isChecked4: false,
+            })
+            :
+            this.setState({
+                isChecked1: false,
+                isChecked2: false,
+                isChecked3: false,
+                isChecked4: true,
+            });
     }
 
     handleOpenModal1() {
@@ -110,7 +184,6 @@ class Cartoon extends Component{
             showModal5: false,
         });
     }
-
     render(){
         const video = [
             'Video1','Video2','Video3','Video4'
@@ -176,28 +249,44 @@ class Cartoon extends Component{
                         </div>
                     </Modal>
                 </IconContext.Provider>
-                <div>
+                <div className={'img1'}>
                     <img src={cartoon1} alt="" type="button" onClick={this.handleOpenModal1}/>
                     <p>코시국 최고의 순간들 -원대호-</p>
                     <span>코로나 시국 속에서 우리가 울고 웃었던 최고의 순간들!</span>
+                    {this.state.isChecked1 ?
+                        <FaHeart className={'clickE1'} onClick={this.onclick1} color={'red'}/>:
+                        <FaRegHeart className={'clickE1'} onClick={this.onclick1} color={'red'}/>
+                    }
                 </div>
-                <div>
+                <div className={'img2'}>
                     <img src={cartoon2} alt="" type="button" onClick={this.handleOpenModal2}/>
                     <p>내가 견뎌낸 Corona Blue -극복왕-</p>
                     <span>corona Blue(코로나로 인한 우울감)를 극복해낸 방법</span>
+                    {this.state.isChecked2 ?
+                        <FaHeart className={'clickE1'} onClick={this.onclick2} color={'red'}/>:
+                        <FaRegHeart className={'clickE1'} onClick={this.onclick2} color={'red'}/>
+                    }
                 </div>
-                <div>
+                <div className={'img3'}>
                     <img src={cartoon3} alt="" type="button" onClick={this.handleOpenModal3}/>
                     <p>돌려다오! 내 학교생활 -gkrry-</p>
                     <span>비대면 수업으로 인한 강탈당한 내 학교생활을 돌려다오!</span>
+                    {this.state.isChecked3 ?
+                        <FaHeart className={'clickE1'} onClick={this.onclick3} color={'red'}/>:
+                        <FaRegHeart className={'clickE1'} onClick={this.onclick3} color={'red'}/>
+                    }
                 </div>
-                <div>
+                <div className={'img4'}>
                     <img src={cartoon4} alt="" type="button" onClick={this.handleOpenModal4}/>
                     <p>코로나 국민지원금 넌 어떻게 쓰니? -kimPro-</p>
                     <span>국민지원금 활용하기 전 알아봐야할 사용 꿀팁!</span>
+                    {this.state.isChecked4 ?
+                        <FaHeart className={'clickE1'} onClick={this.onclick4} color={'red'}/>:
+                        <FaRegHeart className={'clickE1'} onClick={this.onclick4} color={'red'}/>
+                    }
                 </div>
                 <div type="button" onClick={this.handleOpenModal5}>
-                    dnjseoghsdnjseogh
+                    투표하기
                 </div>
             </div>
         );
