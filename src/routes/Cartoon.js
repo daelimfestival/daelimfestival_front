0c22258f15191779d3aae9de0dfdde6c2730c8ac
WonDaeHo <77261907+WonDaeHo@users.noreply.github.com>
2021-10-08 14:22:25 +0900

    2021/10/08
    update

--- src/routes/Cartoon.js
+++ src/routes/Cartoon.js
@@ -1,16 +1,18 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import "./Contest.css";
 import ReactPlayer from 'react-player';
-import cartoon1 from './img/cartoon1.png';
-import cartoon2 from './img/cartoon2.png';
-import cartoon3 from './img/cartoon3.png';
-import cartoon4 from './img/cartoon4.png';
+import cartoon1 from '../img/cartoon1.png';
+import cartoon2 from '../img/cartoon2.png';
+import cartoon3 from '../img/cartoon3.png';
+import cartoon4 from '../img/cartoon4.png';
 import Modal from 'react-modal';
 import { AiOutlineClose } from "react-icons/ai";
 import { FaRegHeart } from "react-icons/fa";
 import { FaHeart } from "react-icons/fa";
 import { IconContext } from "react-icons";
 import loginModal from "../common/loginModal";
+import Button from 'react-bootstrap/Button';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const modalStyles = {
     overlay: {
@@ -36,10 +38,6 @@ const modalStyles = {
         backgroundColor: '#EBEBEB',
     }
 }
-
-const modalStyle2 = {
-
-}
 class Cartoon extends Component{
     constructor(props) {
         super(props);
@@ -49,89 +47,27 @@ class Cartoon extends Component{
             showModal3: false,
             showModal4: false,
             showModal5: false,
-            isChecked1: false,
-            isChecked2: false,
-            isChecked3: false,
-            isChecked4: false,
+            radioGroup : {
+                m1: false,
+                m2: false,
+                m3: false,
+                m4: false,
+            }
         };
-
+        this.handleRadio = this.handleRadio.bind(this);
         this.handleOpenModal1 = this.handleOpenModal1.bind(this);
         this.handleOpenModal2 = this.handleOpenModal2.bind(this);
         this.handleOpenModal3 = this.handleOpenModal3.bind(this);
         this.handleOpenModal4 = this.handleOpenModal4.bind(this);
         this.handleOpenModal5 = this.handleOpenModal5.bind(this);
-        this.onclick1 = this.onclick1.bind(this);
-        this.onclick2 = this.onclick2.bind(this);
-        this.onclick3 = this.onclick3.bind(this);
-        this.onclick4 = this.onclick4.bind(this);
         this.handleCloseModal = this.handleCloseModal.bind(this);
     }
-
-    onclick1(){
-        this.state.isChecked1 ?
-            this.setState({
-                isChecked1: false,
-                isChecked2: false,
-                isChecked3: false,
-                isChecked4: false,
-            })
-            :
-            this.setState({
-                isChecked1: true,
-                isChecked2: false,
-                isChecked3: false,
-                isChecked4: false,
-            });
+    handleRadio(event){
+        let obj = {}
+        obj[event.target.value] = event.target.checked
+        this.setState({radioGroup:obj})
+        console.log(obj);
     }
-    onclick2(){
-        this.state.isChecked2 ?
-            this.setState({
-                isChecked1: false,
-                isChecked2: false,
-                isChecked3: false,
-                isChecked4: false,
-            })
-            :
-            this.setState({
-                isChecked1: false,
-                isChecked2: true,
-                isChecked3: false,
-                isChecked4: false,
-            });
-    }
-    onclick3(){
-        this.state.isChecked3 ?
-            this.setState({
-                isChecked1: false,
-                isChecked2: false,
-                isChecked3: false,
-                isChecked4: false,
-            })
-            :
-            this.setState({
-                isChecked1: false,
-                isChecked2: false,
-                isChecked3: true,
-                isChecked4: false,
-            });
-    }
-    onclick4(){
-        this.state.isChecked4 ?
-            this.setState({
-                isChecked1: false,
-                isChecked2: false,
-                isChecked3: false,
-                isChecked4: false,
-            })
-            :
-            this.setState({
-                isChecked1: false,
-                isChecked2: false,
-                isChecked3: false,
-                isChecked4: true,
-            });
-    }
-
     handleOpenModal1() {
         this.setState({
             showModal1: true,
@@ -174,7 +110,6 @@ class Cartoon extends Component{
             showModal5: true,
         });
     }
-
     handleCloseModal() {
         this.setState({
             showModal1: false,
@@ -185,13 +120,8 @@ class Cartoon extends Component{
         });
     }
     render(){
-        const video = [
-            'Video1','Video2','Video3','Video4'
-        ];
-        /*const contentList=video.map((videos)=>(<div className="contentList">{videos}</div>
-        ));*/
         return (
-            <div class="contestBox">
+            <form class="contestBox">
                 <IconContext.Provider value={{
                     style: {
                         width: "100%",
@@ -214,9 +144,9 @@ class Cartoon extends Component{
                         onRequestClose={this.handleCloseModal}
                         ariaHideApp={false}
                     >
-                        {/*<div className="closebtn">
+                        <div className="closebtn">
                             <AiOutlineClose onClick={this.handleCloseModal} />
-                        </div>*/}
+                        </div>
                     </Modal>
                     <Modal
                         isOpen={this.state.showModal3}
@@ -253,42 +183,70 @@ class Cartoon extends Component{
                     <img src={cartoon1} alt="" type="button" onClick={this.handleOpenModal1}/>
                     <p>코시국 최고의 순간들 -원대호-</p>
                     <span>코로나 시국 속에서 우리가 울고 웃었던 최고의 순간들!</span>
-                    {this.state.isChecked1 ?
-                        <FaHeart className={'clickE1'} onClick={this.onclick1} color={'red'}/>:
-                        <FaRegHeart className={'clickE1'} onClick={this.onclick1} color={'red'}/>
-                    }
+                    <input
+                        className={'clickE1'}
+                        type="radio"
+                        name={'radioGroup'}
+                        value={'m1'}
+                        checked={this.state.radioGroup['m1']}
+                        onChange={this.handleRadio}
+                    />
                 </div>
                 <div className={'img2'}>
                     <img src={cartoon2} alt="" type="button" onClick={this.handleOpenModal2}/>
                     <p>내가 견뎌낸 Corona Blue -극복왕-</p>
                     <span>corona Blue(코로나로 인한 우울감)를 극복해낸 방법</span>
-                    {this.state.isChecked2 ?
-                        <FaHeart className={'clickE1'} onClick={this.onclick2} color={'red'}/>:
-                        <FaRegHeart className={'clickE1'} onClick={this.onclick2} color={'red'}/>
-                    }
+                    <input
+                        className={'clickE1'}
+                        type="radio"
+                        name={'radioGroup'}
+                        value={'m2'}
+                        checked={this.state.radioGroup['m2']}
+                        onChange={this.handleRadio}
+                    />
                 </div>
                 <div className={'img3'}>
-                    <img src={cartoon3} alt="" type="button" onClick={this.handleOpenModal3}/>
+                    <img
+                        src={cartoon3}
+                        alt=""
+                        type="button"
+                        onClick={this.handleOpenModal3}
+                    />
                     <p>돌려다오! 내 학교생활 -gkrry-</p>
                     <span>비대면 수업으로 인한 강탈당한 내 학교생활을 돌려다오!</span>
-                    {this.state.isChecked3 ?
-                        <FaHeart className={'clickE1'} onClick={this.onclick3} color={'red'}/>:
-                        <FaRegHeart className={'clickE1'} onClick={this.onclick3} color={'red'}/>
-                    }
+                    <input
+                        className={'clickE1'}
+                        type="radio"
+                        name={'radioGroup'}
+                        value={'m3'}
+                        checked={this.state.radioGroup['m3']}
+                        onChange={this.handleRadio}
+                    />
                 </div>
                 <div className={'img4'}>
-                    <img src={cartoon4} alt="" type="button" onClick={this.handleOpenModal4}/>
+                    <img
+                        src={cartoon4}
+                        alt=""
+                        type="button"
+                        onClick={this.handleOpenModal4}
+                    />
                     <p>코로나 국민지원금 넌 어떻게 쓰니? -kimPro-</p>
                     <span>국민지원금 활용하기 전 알아봐야할 사용 꿀팁!</span>
-                    {this.state.isChecked4 ?
-                        <FaHeart className={'clickE1'} onClick={this.onclick4} color={'red'}/>:
-                        <FaRegHeart className={'clickE1'} onClick={this.onclick4} color={'red'}/>
-                    }
+                    <input
+                        className={'clickE1'}
+                        type="radio"
+                        name={'radioGroup'}
+                        value={'m4'}
+                        checked={this.state.radioGroup['m4']}
+                        onChange={this.handleRadio}
+                    />
                 </div>
-                <div type="button" onClick={this.handleOpenModal5}>
-                    투표하기
+                <div className="d-grid gap-2">
+                    <Button type="submit" variant="secondary" size="lg">
+                        투표하기
+                    </Button>
                 </div>
-            </div>
+            </form>
         );
     }
 }
--- src/routes/Intro.js
+++ src/routes/Intro.js
@@ -1,34 +1,16 @@
 import React, { Component, useRef, useState } from "react";
 import ReactPlayer from 'react-player'
 import "./Intro.css";
-import poster1 from './img/poster1.png';
-import Moon from './img/pngegg.png';
-import sky from './img/darksky.png';
-import symbol from './img/symbol2.png';
+import poster1 from '../img/poster1.png';
+import sele from '../img/sele.png';
 import { TiStarburstOutline } from 'react-icons/ti';
 import "animate.css";
-// Import Swiper React components
-import { Swiper, SwiperSlide } from 'swiper/react';
-
-
-
-// import Swiper core and required modules
-import SwiperCore, {
-    EffectCards
-} from 'swiper';
-
-
-
-
-
 
 class intro extends Component{
     render(){
-
         return(
             <div className={'main_wrap'}>
-
-                <div className="star animate__zoomOut">
+                <div className={'sky'}>
                     <TiStarburstOutline color="#fff" className={"far fa-star star1"}/>
                     <TiStarburstOutline color="#fff" className={"far fa-star star2"}/>
                     <TiStarburstOutline color="#fff" className={"far fa-star star3"}/>
@@ -69,31 +51,94 @@ class intro extends Component{
                     <TiStarburstOutline color="#fff" className={"far fa-star star38"}/>
                     <TiStarburstOutline color="#fff" className={"far fa-star star39"}/>
                     <TiStarburstOutline color="#fff" className={"far fa-star star40"}/>
-                    <h1 className={'HALLO'}>&nbsp;HALLO!</h1>
-                    <p>:DAELIM FESTIVAL</p>
-                    <p>"별이 빛나는 밤에"</p>
-                    <p>2021대림대학교</p>
+                    <TiStarburstOutline color="#fff" className={"far fa-star star41"}/>
+                    <TiStarburstOutline color="#fff" className={"far fa-star star42"}/>
+                    <TiStarburstOutline color="#fff" className={"far fa-star star43"}/>
+                    <TiStarburstOutline color="#fff" className={"far fa-star star44"}/>
+                    <TiStarburstOutline color="#fff" className={"far fa-star star45"}/>
+                    <TiStarburstOutline color="#fff" className={"far fa-star star46"}/>
+                    <TiStarburstOutline color="#fff" className={"far fa-star star47"}/>
+                    <TiStarburstOutline color="#fff" className={"far fa-star star48"}/>
+                    <TiStarburstOutline color="#fff" className={"far fa-star star49"}/>
+                    <TiStarburstOutline color="#fff" className={"far fa-star star50"}/>
+                    <TiStarburstOutline color="#fff" className={"far fa-star star51"}/>
+                    <TiStarburstOutline color="#fff" className={"far fa-star star52"}/>
+                    <TiStarburstOutline color="#fff" className={"far fa-star star53"}/>
+                    <TiStarburstOutline color="#fff" className={"far fa-star star54"}/>
+                    <TiStarburstOutline color="#fff" className={"far fa-star star55"}/>
+                    <TiStarburstOutline color="#fff" className={"far fa-star star56"}/>
+                    <TiStarburstOutline color="#fff" className={"far fa-star star57"}/>
+                    <TiStarburstOutline color="#fff" className={"far fa-star star58"}/>
+                    <TiStarburstOutline color="#fff" className={"far fa-star star59"}/>
+                    <TiStarburstOutline color="#fff" className={"far fa-star star60"}/>
+                    <TiStarburstOutline color="#fff" className={"far fa-star star61"}/>
+                    <TiStarburstOutline color="#fff" className={"far fa-star star62"}/>
+                    <TiStarburstOutline color="#fff" className={"far fa-star star63"}/>
+                    <TiStarburstOutline color="#fff" className={"far fa-star star64"}/>
+                    <TiStarburstOutline color="#fff" className={"far fa-star star65"}/>
+                    <TiStarburstOutline color="#fff" className={"far fa-star star66"}/>
+                    <TiStarburstOutline color="#fff" className={"far fa-star star67"}/>
+                    <TiStarburstOutline color="#fff" className={"far fa-star star68"}/>
+                    <TiStarburstOutline color="#fff" className={"far fa-star star69"}/>
+                    <TiStarburstOutline color="#fff" className={"far fa-star star70"}/>
+                    <TiStarburstOutline color="#fff" className={"far fa-star star71"}/>
+                    <TiStarburstOutline color="#fff" className={"far fa-star star72"}/>
+                    <TiStarburstOutline color="#fff" className={"far fa-star star73"}/>
+                    <TiStarburstOutline color="#fff" className={"far fa-star star74"}/>
+                    <TiStarburstOutline color="#fff" className={"far fa-star star75"}/>
+                    <TiStarburstOutline color="#fff" className={"far fa-star star76"}/>
+                    <TiStarburstOutline color="#fff" className={"far fa-star star77"}/>
+                    <TiStarburstOutline color="#fff" className={"far fa-star star78"}/>
+                    <TiStarburstOutline color="#fff" className={"far fa-star star79"}/>
+                    <TiStarburstOutline color="#fff" className={"far fa-star star80"}/>
                 </div>
-                <div  className={'video'}>
-                    <ReactPlayer url={'https://www.youtube.com/watch?v=L1IWrezMsIA'} playing   controls/>
+                <div className="comment">
+                    <img className={'poster1 animate__animated animate__fadeInLeft'} src={poster1} alt=""/>
+                    <h1>2021대림대학교</h1>
+                    <h2>온라인 가을 축제</h2>
+                    <h3>:DAELIM ONLINE FESTIVAL</h3>
+                    <h4>11.01~11.02</h4>
+                    <h5 className={'animate__animated animate__fadeInUpBig'}>"별이 빛나는 밤에"</h5>
+                    <h6 className={'animate__animated animate__fadeInUpBig'}>맑은 밤하늘을 수 놓을 대림캠퍼스에</h6>
+                    <h7 className={'animate__animated animate__fadeInUpBig'}>여러분이 함께할 비대면 축제! 기대하시라!</h7>
                 </div>
-
-                <img className={'poster1'} src={poster1} alt=""/>
+                <div className={'video'}>
+                    <ReactPlayer
+                        url={'https://www.youtube.com/watch?v=L1IWrezMsIA'}
+                        playing
+                        controls
+                        width={'100%'}
+                        height={'100%'}
+                    />
+                </div>
                 <div className={'text1'}>
-                    <p>AtristLineUp</p>
-                    <p>Guest-part1: 블랙핑크, 소녀시대, 일병 비와이</p>
-                    <p>Guest-part2: 에스파, 한효주, 한지민</p>
+                    <h1>D.F</h1>
+                    <div className={'leftLine'}>
+                        <h2>front 원대호</h2>
+                        <h3>front 이성훈</h3>
+                        <h4>back 서은기</h4>
+                    </div>
                 </div>
-                <img className={'sky'} src={sky} alt=""/>
+                <div className={'celebration'}>
+                    <p>감사합니다!</p>
+                    <h1><b>축하사</b>입니다.</h1>
+                    <h2>
+                        그들의 있을 반짝이는 인생을 가장 사람은 많이 피다. 같은 그것을 때까지 따뜻한 그리하였는가? 꽃이 위하여 무엇을 있는 영락과 청춘의 칼이다. 가치를 이 이성은 것은 꾸며 바이며, 부패뿐이다. 그들의 청춘 보이는 미인을 뭇 없으면 뜨거운지라, 속에 내려온 것이다. 크고 싸인 이상은 역사를 꽃 위하여서. 있는 있는 풀이 끓는 평화스러운 아니다. 구하기 쓸쓸한 인생을 새가 청춘의 구하지 피다. 옷을 위하여서 용감하고 보이는 있는 그들에게 이것이다.
+
+                        얼마나 풀밭에 이것을 위하여서, 천하를 예수는 것이다. 없으면 청춘 풀이 위하여서. 우리는 별과 찬미를 피고 뿐이다. 보내는 천하를 봄바람을 피고, 몸이 얼마나 미인을 크고 있는가? 못할 무엇을 품고 그것은 얼음에 몸이 인류의 듣는다. 것이 눈에 그들의 열락의 소담스러운 밥을 관현악이며, 황금시대다. 무엇을 피어나기 두기 황금시대를 약동하다. 무엇이 가진 그들의 피고 굳세게 앞이 부패뿐이다. 무엇을 몸이 그들은 대고, 없으면, 자신과 때문이다. 꾸며 있는 것이다.보라, 때까지 철환하였는가?
+
+                        이상, 살 우리는 찬미를 풍부하게 풀이 사랑의 구할 때문이다. 위하여, 가지에 끓는 있다. 트고, 있을 이것은 청춘 우리 투명하되 열락의 칼이다. 불러 이것은 자신과 곳이 때에, 그들은 것이 그들에게 청춘에서만 것이다. 실로 군영과 더운지라 그리하였는가? 사라지지 무한한 속잎나고, 꽃 품으며, 실로 듣는다. 못하다 불러 끓는 사랑의 이것을 것이다. 착목한는 그림자는 밥을 그리하였는가? 살았으며, 같이, 그들의 따뜻한 청춘의 두기 무엇을 과실이 피다. 커다란 뛰노는 얼마나 피어나기 관현악이며, 반짝이는 교향악이다.
+                    </h2>
+                    <img src={sele} alt=""/>
+                </div>
                 <div className={'link'}>
                     <a href={'https://www.daelim.ac.kr/intro.do'}>대림대학교 더 살펴보기 click</a>
                 </div>
             </div>
-
         )
     }
 
-}
+
 
 
 export default intro;
