7084fbfff4a66215d2dc67b67ef9063292905c1b
leesh9098 <leesh9098@naver.com>
2021-10-17 20:20:19 +0900

    Daelim Festival

--- src/routes/GuestBook.js
+++ src/routes/GuestBook.js
@@ -6,62 +6,54 @@ import { IconContext } from "react-icons";
 import { FaCommentAlt } from "react-icons/fa";
 import './GuestBook.css'
 import Button from 'react-bootstrap/Button';
-// import InfiniteScroll from 'react-infinite-scroller';
+import InfiniteScroll from "react-infinite-scroll-component";
+import axios from 'axios';
 
-// const api = "";
-// const limit = 4;
+const API_LINK = "http://52.79.141.166/action/board/guest_book_data.php";
 
 class GuestBook extends React.Component {
     state = {
-        productList: [],
         nickname: "익명", //닉네임 값
-        preItems: 0,
-        items: 25, //댓글 값
         token: sessionStorage.getItem("token"),
-        // offset: 0,
-        // isLoading: false
+        list_data: [],
+        total: 0,
+        page: 1,
+        more_data: true
+    };
+
+    componentDidMount() {
+        this.fetchData()
+    }
+
+    fetchData = async () => {
+        let current_url = location.href;
+
+        let json_data = {
+            current_url,
+            token: this.state.token,
+            page: this.state.page
+        };
+
+        let json = btoa(encodeURIComponent(JSON.stringify(json_data)));
+
+        await axios.post(
+            API_LINK,
+            {
+                json: json
+            }
+        ).then(res =>
+            this.setState({
+                total: this.state.total === 0 ? res.data.total : 0,
+                list_data: [...this.state.list_data, ...res.data.list],
+                page: this.state.page + 1
+            })
+        );
     };
 
     commentRegist() {
         if (!this.state.token) {
             alert("로그인 후에 이용이 가능합니다.")
             location.href = "/Login"
-        }
-    }
-
-    // loadFunc = () => {
-    //     fetch(`${api}/ootds?offset=${this.state.offset}&limit=${limit}`)
-    //         .then((res) => res.json())
-    //         .then((res) => {
-    //             this.setState({
-    //                 cards: [...this.state.cards, ...res.ootd_list],
-    //                 offset: this.state.offset + limit,
-    //             });
-    //         })
-    // }
-
-    componentDidMount() {
-        fetch("http://52.79.141.166/action/board/guest_book_data.php")
-        .then((res) => res.json())
-        .then((res) => {
-            let result = res.data.list.slice(this.state.preItems, this.state.items);
-            this.setState({
-                productList: [...this.state.productList, ...result],
-            });
-        });
-        window.addEventListener("scroll", this.InfiniteScroll)
-    }
-
-    InfiniteScroll = () => {
-        let scrollHeight = document.documentElement.scrollHeight;
-        let scrollTop = document.documentElement.scrollTop;
-        let clientHeight = document.documentElement.clientHeight;
-        if (scrollTop + clientHeight >= scrollHeight * 0.95) {
-            this.setState({
-                preItems: this.state.items,
-                items: this.state.items + 25
-            });
-            this.componentDidMount();
         }
     }
 
@@ -79,7 +71,7 @@ class GuestBook extends React.Component {
                     <Form onSubmit={this.onSubmit}>
                         <Form.Group>
                             <p className="nickname">{this.state.nickname}</p>
-                            <Form.Control className="input_text" type="text" placeholder="내용을 입력하세요"/>
+                            <Form.Control className="input_text" type="text" placeholder="내용을 입력하세요" />
                             <div className="regist_button_wrap">
                                 <Button className="regist_button" variant="primary" type="submit" onClick={() => this.commentRegist()}>
                                     등록
@@ -97,19 +89,23 @@ class GuestBook extends React.Component {
                                     <FaCommentAlt />
                                 </div>
                             </IconContext.Provider>
-                            <p className="comment_length">{this.state.items}</p>
+                            <p className="comment_length">{this.state.total}</p>
                         </div>
-                        {/* <InfiniteScroll
-                            pageStart={0}
-                            loadMore={this.loadFunc}
-                            hasMore={true || false}
-                            loader={<div className="loader" key={0} />}
-                            useWindow={false}
-                        >
-                            <p className="comments">test</p>
-                        </InfiniteScroll> */}
                     </Form>
                 </div>
+                <InfiniteScroll
+                    dataLength={this.state.list_data.length} //This is important field to render the next data
+                    next={this.fetchData}
+                    more_data={this.state.more_data}
+                    loader={<h4>Loading...</h4>}
+                >
+                    {this.state.list_data.map(brewery => (
+                        <ul className="user" key={brewery.idx}>
+                            <li>내용: {brewery.content}</li>
+                            <li>작성날짜: {brewery.write_date}</li>
+                        </ul>
+                    ))}
+                </InfiniteScroll>
             </div>
         )
     }
